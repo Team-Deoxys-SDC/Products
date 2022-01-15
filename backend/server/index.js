424f7f5f2dcd9627bbd9f31521816b3b4a24cac3
Ezra Pullido <ezra.pullido@gmail.com>
2022-01-14 19:23:13 -0500

    Fleshed out responses

--- backend/server/index.js
+++ backend/server/index.js
@@ -14,6 +14,7 @@ app.get('/products', (req, res) => {
       res.sendStatus(400);
       return;
     } else {
+      console.log(data)
       res.send(data);
     }
 
@@ -27,20 +28,40 @@ app.get('/products/:product_id', (req, res) => {
       res.sendStatus(400);
       return;
     } else {
-      res.send(data);
+      dbConn.query('SELECT feature, value FROM features where product_id=?', [req.params.product_id], (err, data2) => {
+        if (err) {
+          console.log('ERROR');
+          res.sendStatus(400);
+          return;
+        } else {
+          data[0].features = data2;
+          res.send(data);
+        }
+
+      })
     }
 
   })
 })
 
 app.get('/products/:product_id/styles', (req, res) => {
-  dbConn.query('SELECT * FROM styles where product_id=?', [req.params.product_id], (err, data) => {
+  dbConn.query('SELECT * FROM product where id=?', [req.params.product_id], (err, data) => {
     if (err) {
       console.log('ERROR');
       res.sendStatus(400);
       return;
     } else {
-      res.send(data);
+      dbConn.query('SELECT * FROM styles where product_id=?', [req.params.product_id], (err, data2) => {
+        if (err) {
+          console.log('ERROR');
+          res.sendStatus(400);
+          return;
+        } else {
+          data[0].result = data2;
+          res.send(data);
+        }
+
+      })
     }
 
   })
